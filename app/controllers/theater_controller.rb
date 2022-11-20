b24aa3e86e533d242cdf9a85f4e4047f74303ced
CodeIgor31 <113387973+CodeIgor31@users.noreply.github.com>
2022-11-21 00:25:05 +0300

    Updated a bit

--- app/controllers/theater_controller.rb
+++ app/controllers/theater_controller.rb
@@ -3,16 +3,16 @@ class TheaterController < ApplicationController
   end
 
   def create
-    @temp = []
+    @row = []
     @status = true
 
     begin
-      #performances_filling
-      #actors_filling
-      #guest_filling
-      #def_filling
-      #sostav_filling
-      tickets_filling
+      #performances_fill
+      #actors_fill
+      #guest_fill
+      #def_fill
+      #sostav_fill
+      tickets_fill
 
     rescue Exception => e
       @status = false
@@ -22,35 +22,35 @@ class TheaterController < ApplicationController
 
   private
 
-  def performances_filling
-    1000000.times { @temp << { name: Faker::Book.title, producer: Faker::Name.name, play_date: Faker::Date.between(from: '2022-11-23', to: '2026-09-25'), gend: Faker::Book.genre, sost_id: rand(1..50000)} }
-    Performance.insert_all(@temp)
+  def performances_fill
+    1000000.times { @row << { name: Faker::Book.title, producer: Faker::Name.name, play_date: Faker::Date.between(from: '2022-11-23', to: '2026-09-25'), gend: Faker::Book.genre, sost_id: rand(1..50000)} }
+    Performance.insert_all(@row)
   end
 
-  def actors_filling
-    1000000.times { @temp << { fio: Faker::Name.name, info: col_json, sostav_id: rand(1..50000)}}
-    Actor.insert_all(@temp)
+  def actors_fill
+    1000000.times { @row << { fio: Faker::Name.name, info: col_json, sostav_id: rand(1..50000)}}
+    Actor.insert_all(@row)
   end
 
-  def guest_filling
-    1000000.times { @temp << { fio: Faker::Name.name, tick_id: rand(1..100000000)} }
-    SpecialGuest.insert_all(@temp)
+  def guest_fill
+    1000000.times { @row << { fio: Faker::Name.name, tick_id: rand(1..100000000)} }
+    SpecialGuest.insert_all(@row)
   end
 
-  def def_filling
-    1000000.times { @temp << { desc: Faker::Lorem.paragraph, perf_id: rand(1..1000000)} }
-    Definition.insert_all(@temp)
+  def def_fill
+    1000000.times { @row << { desc: Faker::Lorem.paragraph, perf_id: rand(1..1000000)} }
+    Definition.insert_all(@row)
   end
 
 
-  def sostav_filling
-    50000.times { @temp << { name: Faker::Team.name} }
-    SostavSec.insert_all(@temp)
+  def sostav_fill
+    50000.times { @row << { name: Faker::Team.name} }
+    SostavSec.insert_all(@row)
   end
 
-  def tickets_filling
-    2000000.times { @temp << { cost: rand(5000..100000), performance: rand(1..1000000), row_place: [rand(1..1000), rand(1..1000)]} }
-    Ticket.insert_all(@temp)
+  def tickets_fill
+    2000000.times { @row << { cost: rand(5000..100000), performance: rand(1..1000000), row_place: [rand(1..1000), rand(1..1000)]} }
+    Ticket.insert_all(@row)
   end
   
 
